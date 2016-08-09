5f77277e8fb029148731af4877f8146b1f1e4078
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-09 08:28:55 -0400

    getting data to show up

--- js/app.js
+++ js/app.js
@@ -81,7 +81,7 @@ var getUnanswered = function(tags) {
 };
 
 var topPeeps = function(tag) {
-	tagged_url = 'http://api.stackexchange.com/2.2/tags/' + tag + '/top-answerers/'
+	tagged_url = 'http://api.stackexchange.com/2.2/tags/' + tag + '/top-answerers/all_time'
 		// the parameters we need to pass in our request to StackOverflow's API
 	var request = {
 		// tagged: tags,
@@ -97,15 +97,16 @@ var topPeeps = function(tag) {
 		type: "GET",
 	})
 	.done(function(result){ //this waits for the ajax to return with a succesful promise object
-		var searchResults = showSearchResults(request.tagged, result.items.length);
+		console.log(result.items);
+		// var searchResults = showSearchResults(request.tagged, result.items.length);
 
-		$('.search-results').html(searchResults);
-		//$.each is a higher order function. It takes an array and a function as an argument.
-		//The function is executed once for each item in the array.
-		$.each(result.items, function(i, item) {
-			var question = showQuestion(item);
-			$('.results').append(question);
-		});
+		// $('.search-results').html(searchResults);
+		// //$.each is a higher order function. It takes an array and a function as an argument.
+		// //The function is executed once for each item in the array.
+		// $.each(result.items, function(i, item) {
+		// 	var question = showQuestion(item);
+		// 	$('.results').append(question);
+		// });
 	})
 	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
 		var errorElem = showError(error);
